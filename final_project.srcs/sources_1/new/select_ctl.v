b69300ccbb39f6fbcaa57dc6e595ec48454b1876
Kwang <bruce1198@gmail.com>
2018-06-16 10:02:34 +0800

    select recovered

--- final_project.srcs/sources_1/new/select_ctl.v
+++ final_project.srcs/sources_1/new/select_ctl.v
@@ -45,10 +45,22 @@ module select_ctl(
     always@*
         if((key_down[last_change] == 1'b1) && (key_valid == 1'b1) && turn!=0)
             casex(last_change)
-                9'h75: position_y_next = (position_y==1)?position_y:position_y-1; //up
-                9'h72: position_y_next = (position_y==9)?position_y:position_y+1; //down
-                9'h6B: position_x_next = (position_x==1)?position_x:position_x-1; //left
-                9'h74: position_x_next = (position_x==9)?position_x:position_x+1; //right
+                9'h75: begin
+                    position_y_next =  (position_y==1)?position_y:position_y-1; //up
+                    position_x_next = position_x;
+                end
+                9'h72: begin
+                    position_x_next = position_x;
+                    position_y_next = (position_y==9)?position_y:position_y+1; //down
+                end
+                9'h6B: begin
+                    position_x_next = (position_x==1)?position_x:position_x-1; //left
+                    position_y_next = position_y;
+                end
+                9'h74: begin
+                    position_x_next = (position_x==9)?position_x:position_x+1; //right
+                    position_y_next = position_y;
+                end
                 default: begin
                     position_x_next = position_x;
                     position_y_next = position_y;
